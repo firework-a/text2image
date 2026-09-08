216cd36248e714565acfc1f4a9d70689454f23f1
firework-a <2737459675@qq.com>
2026-09-09 00:30:35 +0800

    feat: 修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
       entry: './src/index.ts',
-      name: 'textImage',
+      name: 'text2image',
       formats: ['umd', 'iife', 'es'],
     },
   },
+  plugins: [
+    dts({
+      tsconfigPath: './tsconfig.json',
+      outDir: './dist',
+      include: ['src/**/*.ts'],
+    }),
+  ],
 });
